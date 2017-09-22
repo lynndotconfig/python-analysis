ee85c58e508c6e5d6e459de2e8fc451d5465ad68
lynndotconfig <lynn.config@gmail.com>
2017-09-22 19:02:03 +0800

    [fix] network

---
+++ network/diesel-usage.py
@@ -0,0 +1,34 @@
+import diesel
+import argparse
+
+
+class EchoServer(object):
+    """A echo server using diesel."""
+
+    def handle(self, remote_addr):
+        """Runs the echo server."""
+        host, port = remote_addr[0], remote_addr[1]
+        print 'Echo client connected from: %s:%d' % (host, port)
+
+        while True:
+            try:
+                message = diesel.until_eol()
+                your_message = ':'.join(['You said', message])
+                diesel.send(your_message)
+            except Exception, e:
+                print 'Exception:', e
+
+
+def main(server_port):
+    app = diesel.Application()
+    server = EchoServer()
+    app.add_service(diesel.Service(server.handler, server_port))
+    app.run()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Echo server example with diesel.')
+    parser.add_argument('--port', action='store', dest='port', type=int, required=True)
+    given_args = parser.parse_args()
+    port = given_args.port
+    main(port)
--- network/select-usage.py
+++ network/select-usage.py
@@ -10,6 +10,7 @@ import argparse
 SERVER_HOST = 'localhost'
 CHAT_SERVER_NAME = 'server'
 
+
 # Same utilities
 def send(channel, *args):
     buffer = cPickle.dumps(args)
@@ -18,12 +19,14 @@ def send(channel, *args):
     channel.send(size)
     channel.send(buffer)
 
+
 def receive(channel):
     size = struct.calcsize("L")
     size = channel.recv(size)
     try:
         size = socket.ntohl(struct.unpack("L", size)[0])
     except struct.error, e:
+        print e
         return ''
     buf = ""
     while len(buf) < size:
@@ -32,6 +35,7 @@ def receive(channel):
 
 
 class ChatServer(object):
+
     """An example chat server using select."""
     def __init__(self, port, backlog=5):
         self.clients = 0
@@ -47,45 +51,53 @@ class ChatServer(object):
         signal.signal(signal.SIGINT, self.sighandler)
 
     def sighandler(self, signum, frame):
-          """Clean up client outputs."""
-          # Close the server
-          print 'Shutting down server...'
-          # Close existing client sockets
-          for output in self.outputs:
-              output.close()
-          self.server.close()
-    
+        """Clean up client outputs."""
+        # Close the server
+        print 'Shutting down server...'
+        # Close existing client sockets
+        for output in self.outputs:
+            output.close()
+        self.server.close()
+
     def get_client_name(self, client):
         """Return the name of the client."""
         info = self.clientmap[client]
         host, name = info[0][0], info[1]
         return '@'.join((name, host))
-    
+
     def run(self):
+        """Run"""
         inputs = [self.server, sys.stdin]
+        print 'inputs:', inputs
         self.outputs = []
         running = True
         while running:
             try:
                 readable, writeable, exceptional = select.select(inputs, self.outputs, [])
-            except (socket.error, select.error), e:
+            except socket.error, e:
+                print 'Socket Error: %s' % e
+                break
+            except select.error, e:
+                print 'Select error: ', e
                 break
             for sock in readable:
                 if sock == self.server:
                     # handle the server socket
                     client, address = self.server.accept()
-                    print "Chat server: got conn %d from %s" %(client.fileno(), address)
+                    print "Chat server: got conn %d from %s" % (
+                        client.fileno(), address)
                     # Read the login name
                     cname = receive(client).split('NAME: ')[1]
                     # Compute client name and send back
                     self.clients += 1
-                    send(client, 'CLIENT: ', + str(address[0]))
+                    send(client, 'CLIENT: ' + str(address[0]))
                     inputs.append(client)
                     self.clientmap[client] = (address, cname)
                     # Send joining information to other clients
-                    msg = "\n(Connected: New client (%d) from %s)" %(self.clients, self.get_client_name(client))
+                    msg = "\n(Connected: New client (%d) from %s)" % (
+                        self.clients, self.get_client_name(client))
 
-                    for output in outputs:
+                    for output in self.outputs:
                         send(output, msg)
                     self.outputs.append(client)
                 elif sock == sys.stdin:
@@ -110,13 +122,14 @@ class ChatServer(object):
                             inputs.remove(sock)
                             self.outputs.remove(sock)
                             # Sending client leaving info to others
-                            msg = "\n(Now hung up: Client from %s)" % self.get_client_name(sock)
+                            msg = "\n(Now hung up: Client from %s)" \
+                                % self.get_client_name(sock)
                             for output in self.outputs:
                                 send(output, msg)
                     except socket.error, e:
-                      # Remove
-                      inputs.remove(sock)
-                      self.outputs.remove(sock)
+                        # Remove
+                        inputs.remove(sock)
+                        self.outputs.remove(sock)
         self.server.close()
 
 
@@ -145,28 +158,28 @@ class ChatClient(object):
         except socket.error, e:
             print "Failed to connect to chat server @ port %d" % self.port
             sys.exit(1)
-    
+
     def run(self):
         """Chat client main loop"""
         while self.connected:
             try:
-                  sys.stdout.write(self.prompt)
-                  sys.stdout.flush()
-                  # Wait for inpt from stdin and socket
-                  readable, writeable, exceptional = select.select([0, self.sock], [], [])
-                  for sock in readable:
-                      if sock == 0:
-                          data = sys.stdin.readline().strip()
-                          if data: send(self.sock, data)
-                      elif sock == self.sock:
-                          data = receive(self.sock)
-                          if not data:
-                              print "Client shutting down."
-                              self.connected = False
-                              break
-                          else:
-                              sys.stdout.write(data + '\n')
-                              sys.stdout.flush()
+                sys.stdout.write(self.prompt)
+                sys.stdout.flush()
+                # Wait for inpt from stdin and socket
+                readable, writeable, exceptional = select.select([0, self.sock], [], [])
+                for sock in readable:
+                    if sock == 0:
+                        data = sys.stdin.readline().strip()
+                        if data: send(self.sock, data)
+                    elif sock == self.sock:
+                        data = receive(self.sock)
+                        if not data:
+                            print "Client shutting down."
+                            self.connected = False
+                            break
+                        else:
+                            sys.stdout.write(data + '\n')
+                            sys.stdout.flush()
             except KeyboardInterrupt:
                 print " Client interrupted. "
                 self.sock.close()
