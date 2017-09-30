b395c988994c5d8495c19daa54fec609b56bb6e7
lynndotconfig <lynn.config@gmail.com>
2017-09-30 14:19:36 +0800

    [add] mq queue

---
+++ mq/create-queue.py
@@ -0,0 +1,31 @@
+import pika
+
+credentials = pika.PlainCredentials("guest", "guest")
+conn_params = pika.ConnectionParameters("localhost", credentials=credentials)
+
+conn_broker = pika.BlockingConnection(conn_params)  # connect to server
+channel = conn_broker.channel()  # get channel
+
+# declare exchange, if not exist, then create it, else go on;
+channel.exchange_declare(
+    exchange="logs-exchange",
+    exchange_type="topic",
+    passive=False,
+    durable=True,
+    auto_delete=False
+)
+
+channel.queue_declare(queue="msg-inbox-errors")  # declare queue
+channel.queue_declare(queue="msg-inbox-logss")  # declare queue
+channel.queue_declare(queue="all-logs")  # declare queue
+
+channel.queue_bind(
+    queue="msg-inbox-errors",
+    exchange="logs-exchange",
+    routing_key="error.msg-inbox"
+)
+channel.queue_bind(
+    queue="msg-inbox-logs",
+    exchange="logs-exchange",
+    routing_key="*.msg-inbox"
+)
